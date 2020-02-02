85b85e0083154b4287d81e4eaf6a9af63b9c6987
andrei_kuturmazov <andrei.kuturmazov@gmail.com>
2020-02-02 14:37:18 +0300

    writer close

--- src/Servlet.java
+++ src/Servlet.java
@@ -4,17 +4,16 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
-import java.io.PrintWriter;
 
 @WebServlet("/hello")
 public class Servlet extends HttpServlet {
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-        try(PrintWriter writer = resp.getWriter()) {
-            resp.setContentType("text/html");
-            writer.println("<html><body>");
-            writer.println("<h2>Hello world!</h2>");
-            writer.println("</html></body>");
-        }
+
+        resp.setContentType("text/html");
+        resp.getWriter().println("<html><body>");
+        resp.getWriter().println("<h2>Hello world!</h2>");
+        resp.getWriter().println("</html></body>");
+        resp.getWriter().close();
     }
 }
